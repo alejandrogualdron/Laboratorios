105beac491903c65ee8b0253bc8981a267f19e7c
alejandrogualdron <52297856+alejandrogualdron@users.noreply.github.com>
2020-04-04 01:16:40 +0200

    Add files via upload

---
+++ CRUD_ESTUDIANTE_2020_2-ejb/build/generated-sources/ap-source-output/co/edu/unipiloto/CRUD_ESTUDIANTE/entity/Estudiantes_.java
@@ -0,0 +1,16 @@
+package co.edu.unipiloto.CRUD_ESTUDIANTE.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-02T14:08:50")
+@StaticMetamodel(Estudiantes.class)
+public class Estudiantes_ { 
+
+    public static volatile SingularAttribute<Estudiantes, Integer> studentid;
+    public static volatile SingularAttribute<Estudiantes, String> firstname;
+    public static volatile SingularAttribute<Estudiantes, Integer> yearlevel;
+    public static volatile SingularAttribute<Estudiantes, String> lastname;
+
+}
